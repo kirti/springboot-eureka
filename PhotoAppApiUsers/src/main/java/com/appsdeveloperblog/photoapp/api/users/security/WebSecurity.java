c4f93309dc9e148ea77fb0dead60f02d190f317b
Kirti kaushal <kirtikaushal@Kirtis-MacBook-Pro.local>
2024-05-29 13:11:14 -0400

    Users Micro-services eureka implementation with login and create

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
@@ -1,29 +1,63 @@
 package com.appsdeveloperblog.photoapp.api.users.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.appsdeveloperblog.photoapp.api.users.service.UsersService;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity {
 	
+	private Environment environment;
+	private UsersService usersService;
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@Autowired
+	public WebSecurity(Environment environment, UsersService usersService, BCryptPasswordEncoder bCryptPasswordEncoder)
+	{
+		this.environment = environment;
+		this.usersService = usersService;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+	}
+	
 
 	@Bean
 	protected SecurityFilterChain configure(HttpSecurity http) throws Exception{
+		
+		
+      AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+      
+      authenticationManagerBuilder.userDetailsService(usersService).passwordEncoder(bCryptPasswordEncoder);
+      
+      AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
 
-
-        http.csrf(csrf -> csrf.disable());
+      
+      
+     AuthenticationFilter authenticationFilter= new AuthenticationFilter(usersService,environment, authenticationManager);
+      
+     authenticationFilter.setFilterProcessesUrl(environment.getProperty("login.url.path"));
+      
+      http.csrf(csrf -> csrf.disable());
 
         http.authorizeHttpRequests(requests -> requests
                 .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                .requestMatchers(HttpMethod.POST, "/users/**").permitAll()
                 .requestMatchers(HttpMethod.GET, "/users/**").permitAll()
                 .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll())
+                .addFilter(authenticationFilter)
+                .authenticationManager(authenticationManager)
                 .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         http.headers((headers) -> headers.disable());
--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/service/UsersServiceImpl.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/service/UsersServiceImpl.java
@@ -1,10 +1,14 @@
 package com.appsdeveloperblog.photoapp.api.users.service;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -47,7 +51,32 @@ public class UsersServiceImpl implements UsersService {
 		
 		return returvalue;
 	}
-	
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		UserEntity userEntity = usersRepository.findByEmail(username);
+		
+		if(userEntity == null) throw new UsernameNotFoundException(username);	
+		
+		return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), true, true, true, true, new ArrayList<>());
+	}
+
+	@Override
+	public UserDto getUserDetailsByEmail(String email) { 
+		UserEntity userEntity = usersRepository.findByEmail(email);
+		
+		if(userEntity == null) throw new UsernameNotFoundException(email);
+		
+		
+		return new ModelMapper().map(userEntity, UserDto.class);
+	}
+
+	@Override
+	public UserDto getUserByUserId(String userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 
 
 }
